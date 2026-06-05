958d323ac322dff80cddb39dc264a154de85ea83
Gvantsa_vakh <gvanca05.vax@gmail.com>
2026-06-05 14:28:57 +0400

    added NatSpec and named mapping params to EncryptedBidBox

--- contracts/EncryptedBidBox.sol
+++ contracts/EncryptedBidBox.sol
@@ -5,11 +5,17 @@ import {FHE, euint32, externalEuint32} from "@fhevm/solidity/lib/FHE.sol";
 import {ZamaEthereumConfig} from "@fhevm/solidity/config/ZamaConfig.sol";
 
 /// @title EncryptedBidBox
+/// @author Your Name
 /// @notice Small learning contract: stores one encrypted bid.
 contract EncryptedBidBox is ZamaEthereumConfig {
-    mapping(address => euint32) private bids;
-    mapping(address => bool) public hasBid;
+    mapping(address bidder => euint32 encryptedBid) private bids;
 
+    /// @notice Tracks whether an address has submitted a bid
+    mapping(address bidder => bool submitted) public hasBid;
+
+    /// @notice Submits an encrypted bid for the caller
+    /// @param inputBid The encrypted bid value
+    /// @param inputProof The proof validating the encrypted input
     function submitBid(externalEuint32 inputBid, bytes calldata inputProof) external {
         euint32 bid = FHE.fromExternal(inputBid, inputProof);
 
@@ -20,6 +26,8 @@ contract EncryptedBidBox is ZamaEthereumConfig {
         FHE.allow(bids[msg.sender], msg.sender);
     }
 
+    /// @notice Returns the encrypted bid for a given bidder
+    /// @param bidder The address of the bidder
     function getEncryptedBid(address bidder) external view returns (euint32) {
         return bids[bidder];
     }
